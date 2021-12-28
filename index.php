6ba3587bae3b7835fb98ee886b0ced5d01e1bc92
Lê Trí Đức <76910943+ltrduc@users.noreply.github.com>
2021-12-28 20:53:08 +0700

    lsa-v2

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/website/views/');
+	header('Location: '.$uri.'/lifeskillsassistanceclub-V2/views/');
 	exit;
 ?>
